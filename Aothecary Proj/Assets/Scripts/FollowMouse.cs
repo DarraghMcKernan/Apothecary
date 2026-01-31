3cf9289fd7ca20a58246d9527a66a6abdbdd505d
DarraghMcKernan <93271170+DarraghMcKernan@users.noreply.github.com>
2026-01-31 19:16:16 +0000

    Fixed drag

--- Aothecary Proj/Assets/Scripts/FollowMouse.cs
+++ Aothecary Proj/Assets/Scripts/FollowMouse.cs
@@ -7,7 +7,7 @@ public class FollowMouse : MonoBehaviour
 {
     public string cameraName = string.Empty;
     private Camera workbenchCamera;
-    private Vector2 clickMouse;
+    private bool isHoldMouse = false;
     private Vector3 worldPos;
     public bool workbenchActive = false;
     [SerializeField] private float sizeObject = 0.5f;
@@ -25,9 +25,14 @@ public class FollowMouse : MonoBehaviour
         mousePos.z = workbenchCamera.nearClipPlane + 6f; // Distance from camera
         worldPos = workbenchCamera.ScreenToWorldPoint(mousePos);
         //if this workbench is active
-        if (workbenchActive == true && Input.GetMouseButton(0) && worldPos.x <= transform.position.x + sizeObject && worldPos.x >= transform.position.x - sizeObject && worldPos.z <= transform.position.z + sizeObject && worldPos.z >= transform.position.z - sizeObject)
+        if (workbenchActive == true && Input.GetMouseButton(0) && worldPos.x <= transform.position.x + sizeObject && worldPos.x >= transform.position.x - sizeObject && worldPos.z <= transform.position.z + sizeObject && worldPos.z >= transform.position.z - sizeObject || isHoldMouse)
         {
             transform.position = new Vector3(worldPos.x, worldPos.y, worldPos.z);
+            isHoldMouse = true;
+            if (!Input.GetMouseButton(0))
+            {
+                isHoldMouse = false;
+            }
         }
 
     }
