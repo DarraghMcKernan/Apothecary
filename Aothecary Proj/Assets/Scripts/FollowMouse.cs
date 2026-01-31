4c86eca75754a76a0d64a6ee87ded31afb1a0f5e
DarraghMcKernan <93271170+DarraghMcKernan@users.noreply.github.com>
2026-01-31 14:54:00 +0000

    Complete drag system

--- Aothecary Proj/Assets/Scripts/FollowMouse.cs
+++ Aothecary Proj/Assets/Scripts/FollowMouse.cs
@@ -1,21 +1,37 @@
+using System.Drawing;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FollowMouse : MonoBehaviour
 {
 
-    public Camera workbenchCamera;
+    [SerializeField] private Camera workbenchCamera;
+    private Vector2 clickMouse;
+    private Vector3 worldPos;
+    [SerializeField] private float sizeObject = 0.5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        workbenchCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 mousePos = Input.mousePosition;
+        
         mousePos.z = workbenchCamera.nearClipPlane + 6f; // Distance from camera
-        Vector3 worldPos = workbenchCamera.ScreenToWorldPoint(mousePos);
-        transform.position = new Vector3(worldPos.x, worldPos.y, worldPos.z);
+        worldPos = workbenchCamera.ScreenToWorldPoint(mousePos);
+        if (Input.GetMouseButton(0) && worldPos.x <= transform.position.x + sizeObject && worldPos.x >= transform.position.x - sizeObject && worldPos.z <= transform.position.z + sizeObject && worldPos.z >= transform.position.z - sizeObject)
+        {
+            transform.position = new Vector3(worldPos.x, worldPos.y, worldPos.z);
+
+        }
+
+    }
+    public void ClickOnObject(InputAction.CallbackContext ctx)
+    {
+
     }
 }
