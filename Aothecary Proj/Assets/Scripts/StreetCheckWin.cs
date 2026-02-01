f04f1ccc0b835e3389a1bb1d12fd7da480c168b9
Apple9511 <80068370+Apple9511@users.noreply.github.com>
2026-02-01 11:01:02 +0000

    Added Street Check Win

--- Aothecary Proj/Assets/Scripts/StreetCheckWin.cs
+++ Aothecary Proj/Assets/Scripts/StreetCheckWin.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
+using System.Collections;
+using System.IO;
 
 public class StreetCheckWin : MonoBehaviour
 {
+    public float waitSeconds = 5f;
+    public SwitchScene gameOverSwitchScene;
+    public SwitchScene winSwitchScene;
+
+    private int succcessInt; // 1 = win, 0 = fail
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(checkWin());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator checkWin()
     {
-        
+        yield return new WaitForSeconds(waitSeconds);
+        succcessInt = PlayerPrefs.GetInt("Success");
+
+        switch(succcessInt)
+        {
+            case 0:
+                gameOverSwitchScene.SwapScene();
+                break;
+            case 1:
+                winSwitchScene.SwapScene();
+                break;
+        }
     }
 }
